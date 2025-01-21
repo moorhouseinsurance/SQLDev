85caac67024d6e6e853a52de21c0751814f64a22
LingaNageswara <Linga.Nageswara@constructaquote.com>
2025-01-21 10:35:10 +0000

    8154652828 - Update endorsements on Companion CAR and CPE schemes
    https://constructaquote.monday.com/boards/127927377/views/8221025/pulses/8154652828?notification=5684501976

---
+++ Tickets/2025/8154652828 Update endorsements on Companion CAR and CPE schemes/03.Insert Script_[dbo].[uspSchemeContractorsPlantAndEquipmentTokioMarineKiln].sql
@@ -0,0 +1,55 @@
+USE [Transactor_Live]
+GO
+
+--select * from [SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade]
+
+-- Step1: Added new fields to capture existing records info
+  ALTER TABLE [Transactor_Live].[dbo].[SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade]
+  ADD [Decline] BIT, [EndDateTime] datetime, [InsertDateTime] datetime, [InsertUserID] varchar(50), [UpdateDateTime] datetime, [UpdateUserID] varchar(50);
+
+
+-- Step2: Update existig records enddatetime with todays date
+
+  -- 203 records
+  -- Select * from [Transactor_Live].[dbo].[SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade] where enddatetime is null order by [SchemeContractorsAllRiskTokioMarineKilnTradeID]
+  UPDATE [Transactor_Live].[dbo].[SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade]
+  SET [EndDateTime] = '08 Jan 2025'
+     ,[UpdateDateTime] = GETDATE()
+     ,[UpdateUserID] = 'LINGA'
+
+-- Step3 : Insert new Records with new endorsements
+INSERT INTO [Transactor_Live].[dbo].[SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade]( 
+       [StartDatetime]
+      ,[TradeID]
+      ,[Endorsements]
+      ,[Load]
+      ,[Refer]
+      ,[MaxDepth]
+      ,[MaxHeight]
+	  ,[Decline]
+	  ,[EndDateTime]
+	  ,[InsertDateTime]
+	  ,[InsertUserID]
+	  ,[UpdateDateTime]
+      ,[UpdateUserID]
+)
+SELECT 
+       
+       '08 Jan 2025'
+      ,[TradeID]
+      ,'TMKCAR03'  
+      ,[Load]
+      ,[Refer]
+      ,[MaxDepth]
+      ,[MaxHeight]
+	  ,[Decline]
+	  ,NULL
+	  ,GETDATE()
+	  ,'LINGA'
+	  ,NULL
+	  ,NULL 
+FROM [Transactor_Live].[dbo].[SchemeContractorsPlantAndEquipmentTokioMarineKilnTrade]
+WHERE [StartDatetime] = '29 Apr 2022' AND [UpdateUserID] = 'LINGA'
+ORDER BY SchemeContractorsPlantAndEquipmentTokioMarineKilnTradeID ASC;
+
+
